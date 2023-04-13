1b4f107afecba2429dce0406e4aac0783f3753f1
Aleksey Pi <aleksey.pilyugin@gmail.com>
2023-04-13 10:44:43 +0200

    Reorganizing code to functions

--- content.js
+++ content.js
@@ -13,14 +13,53 @@ document.addEventListener("mousedown", (event) => {
   }
 });
 
+async function getOpenAiSectretKey() {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get("openaiSecretKey", function (data) {
+      let openaiSecretKey = data.openaiSecretKey;
+      if (!openaiSecretKey) {
+        openaiSecretKey = prompt(
+          "Please enter your OpenAI secret key. Enable paid plan here https://platform.openai.com/account/billing/overview and generate secret key here https://platform.openai.com/account/api-keys"
+        );
+        chrome.storage.sync.set({ openaiSecretKey }); //! store it only if success or it should be possible to reset
+      }
+      resolve(openaiSecretKey);
+    });
+  });
+}
+
+async function getBetterText(openaiSecretKey, text) {
+  return fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + openaiSecretKey,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: "Improve this text: " + text }],
+      max_tokens: 1000,
+      temperature: 0,
+      n: 1,
+      stream: false,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      return data.choices[0].message.content;
+    });
+}
+
 document.addEventListener("mouseup", (event) => {
   const selection = window.getSelection();
   if (selection.type !== "Range") return;
   const selectedText = selection.toString();
   if (!selectedText) return;
 
-  const textRange = selection.getRangeAt(0);
-  const textRect = textRange.getBoundingClientRect();
+  const selectionRange = selection.getRangeAt(0);
+  const selectionRect = selectionRange.getBoundingClientRect();
 
   const mouseX = event.clientX;
   const mouseY = event.clientY;
@@ -34,58 +73,34 @@ document.addEventListener("mouseup", (event) => {
   button.style.zIndex = 1;
   document.body.appendChild(button);
 
-  button.addEventListener("mouseup", (event) => {
+  button.addEventListener("mouseup", async (event) => {
     event.stopPropagation();
     document.body.removeChild(button);
     button = null;
 
-    chrome.storage.sync.get("openaiSecretKey", function (data) {
-      let openaiSecretKey = data.openaiSecretKey;
-      if (!openaiSecretKey) {
-        openaiSecretKey = prompt(
-          "Please enter your OpenAI secret key generated here https://platform.openai.com/account/api-keys:"
-        );
-        chrome.storage.sync.set({ openaiSecretKey: openaiSecretKey }); // we need to store it only if success
-      }
+    const openaiSecretKey = await getOpenAiSectretKey();
+    const betterText = await getBetterText(openaiSecretKey, selectedText);
 
-      fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + openaiSecretKey,
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          messages: [
-            { role: "user", content: "Improve this text: " + selectedText },
-          ],
-          max_tokens: 1000,
-          temperature: 0,
-          n: 1,
-          stream: false,
-        }),
-      })
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          const text = data.choices[0].message.content;
+    const x = selectionRect.left;
+    const y = selectionRect.bottom;
 
-          const x = textRect.left;
-          const y = textRect.bottom;
+    textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    textarea.value = betterText;
+    textarea.className = "writeItBetterBox";
+    textarea.style.position = "absolute";
+    textarea.style.left = `${x}px`;
+    textarea.style.top = `${y + 10}px`;
+    textarea.style.width = `${selectionRect.width}px`;
+    textarea.style.height = `${textarea.scrollHeight}px`;
+    textarea.style.overflowY = "hidden";
+    textarea.style.zIndex = 1;
 
-          textarea = document.createElement("textarea");
-          document.body.appendChild(textarea);
-          textarea.value = text;
-          textarea.className = "writeItBetterBox";
-          textarea.style.position = "absolute";
-          textarea.style.left = `${x}px`;
-          textarea.style.top = `${y + 10}px`;
-          textarea.style.width = `${textRect.width}px`;
-          textarea.style.height = `${textarea.scrollHeight}px`;
-          textarea.style.overflowY = "hidden";
-          textarea.style.zIndex = 1;
-        });
+    textarea.addEventListener("mousedown", (event) => {
+      selectionRange.deleteContents();
+      selectionRange.insertNode(document.createTextNode(textarea.value));
+      document.body.removeChild(textarea);
+      textarea = null;
     });
   });
 });
