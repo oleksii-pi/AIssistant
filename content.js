9aff67ebc366a99490c60ab13c8ecb6c88475b57
Aleksey Pi <aleksey.pilyugin@gmail.com>
2023-04-15 10:17:15 +0200

    Do not add and remove buttons and text area, but hide or show them

--- content.js
+++ content.js
@@ -1,16 +1,64 @@
-let button;
-let textarea;
+let button = document.createElement("button");
+button.style.display = "none";
+button.innerText = "W";
+button.className = "writeItBetterButton";
+document.body.appendChild(button);
+
+let textarea = document.createElement("textarea");
+textarea.style.display = "none";
+textarea.className = "writeItBetterBox";
+document.body.appendChild(textarea);
+
+button.addEventListener("mouseup", async (event) => {
+  const selection = window.getSelection();
+  if (selection.type !== "Range") return;
+  const selectedText = selection.toString();
+  if (!selectedText) return;
+
+  const selectionRange = selection.getRangeAt(0);
+  const selectionRect = selectionRange.getBoundingClientRect();
+
+  event.stopPropagation();
+  button.style.display = "none";
+
+  const openaiSecretKey = await getOpenAiSectretKey();
+  const betterText = await getAnswer(
+    openaiSecretKey,
+    "Improve this text: " + selectedText
+  );
+
+  const x = selectionRect.left + window.pageXOffset;
+  const y = selectionRect.bottom + window.pageYOffset;
+
+  textarea.value = betterText;
+  textarea.style.left = `${x}px`;
+  textarea.style.top = `${y + 10}px`;
+  textarea.style.width = `${selectionRect.width}px`;
+  textarea.style.display = "block";
+  textarea.style.height = `${textarea.scrollHeight}px`;
+});
 
 document.addEventListener("mousedown", (event) => {
   if (button && !button.contains(event.target)) {
-    document.body.removeChild(button);
-    button = null;
+    button.style.display = "none";
   }
 
   if (textarea && !textarea.contains(event.target)) {
-    document.body.removeChild(textarea);
-    textarea = null;
+    textarea.style.display = "none";
   }
+});
+
+document.addEventListener("mouseup", (event) => {
+  const selection = window.getSelection();
+  if (selection.type !== "Range") return;
+  const selectedText = selection.toString();
+  if (!selectedText) return;
+
+  const mouseX = event.clientX + window.pageXOffset;
+  const mouseY = event.clientY + window.pageYOffset;
+  button.style.left = `${mouseX + 10}px`;
+  button.style.top = `${mouseY + 10}px`;
+  button.style.display = "block";
 });
 
 async function getOpenAiSectretKey() {
@@ -28,7 +76,7 @@ async function getOpenAiSectretKey() {
   });
 }
 
-async function getBetterText(openaiSecretKey, text) {
+async function getAnswer(openaiSecretKey, text) {
   document.body.style.cursor = "wait";
   return fetch("https://api.openai.com/v1/chat/completions", {
     method: "POST",
@@ -38,7 +86,7 @@ async function getBetterText(openaiSecretKey, text) {
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Improve this text: " + text }],
+      messages: [{ role: "user", content: text }],
       max_tokens: 1000,
       temperature: 0,
       n: 1,
@@ -56,60 +104,3 @@ async function getBetterText(openaiSecretKey, text) {
       document.body.style.cursor = "default";
     });
 }
-
-function createButton(left, top) {
-  const b = document.createElement("button");
-  b.innerText = "Write it better";
-  b.className = "writeItBetterButton";
-  b.style.left = `${left}px`;
-  b.style.top = `${top}px`;
-  document.body.appendChild(b);
-  return b;
-}
-
-function createTextArea(left, top, width, text) {
-  const ta = document.createElement("textarea");
-  document.body.appendChild(ta);
-  ta.value = text;
-  ta.className = "writeItBetterBox";
-  ta.style.left = `${left}px`;
-  ta.style.top = `${top}px`;
-  ta.style.width = `${width}px`;
-  ta.style.height = `${ta.scrollHeight}px`;
-  return ta;
-}
-
-document.addEventListener("mouseup", (event) => {
-  const selection = window.getSelection();
-  if (selection.type !== "Range") return;
-  const selectedText = selection.toString();
-  if (!selectedText) return;
-
-  const selectionRange = selection.getRangeAt(0);
-  const selectionRect = selectionRange.getBoundingClientRect();
-
-  const mouseX = event.clientX + window.pageXOffset;
-  const mouseY = event.clientY + window.pageYOffset;
-  button = createButton(mouseX + 10, mouseY + 10);
-
-  button.addEventListener("mouseup", async (event) => {
-    event.stopPropagation();
-    document.body.removeChild(button);
-    button = null;
-
-    const openaiSecretKey = await getOpenAiSectretKey();
-    const betterText = await getBetterText(openaiSecretKey, selectedText);
-
-    const x = selectionRect.left + window.pageXOffset;
-    const y = selectionRect.bottom + window.pageYOffset;
-
-    textarea = createTextArea(x, y + 10, selectionRect.width, betterText);
-
-    // textarea.addEventListener("mousedown", () => {
-    //   selectionRange.deleteContents();
-    //   selectionRange.insertNode(document.createTextNode(textarea.value));
-    //   document.body.removeChild(textarea);
-    //   textarea = null;
-    // });
-  });
-});
